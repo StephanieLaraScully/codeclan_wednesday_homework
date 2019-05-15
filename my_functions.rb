9971cc5ecfd95932028f19ad867c7f3db29cf0cb
Jeremy Wood <jeremy.wood.nz@gmail.com>
2019-05-15 16:13:27 +0100

    initial commit

---
+++ my_functions.rb
@@ -0,0 +1,65 @@
+def add_array_lengths(array1, array2)
+  array1.length + array2.length
+end
+
+def sum_array(numbers)
+  numbers.sum
+  # total=0
+  # for number in numbers
+  #   total+=number
+  # end
+  # return total
+end
+
+def find_item(array, value)
+  array.include?(value)
+  # array.each do |x|
+  #   if value == x
+  #     return true
+  #   end
+  # end
+  # return false
+end
+
+def get_first_key(hash_name)
+  hash_name[0]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def add_array_lengths(array1,array2)
+#   array1.length+array2.length
+# end
+#
+#
+# sum_array
+# .sum
+#
+# find_item
+# .index!=nil
+#
+#
+# get_first_key
+# .first
